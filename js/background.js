20353abaa53783f7ba03a761069be32d62be617c
Don Kim <kim.don2@husky.neu.edu>
2021-07-23 16:44:50 -0400

    Forgot to add comments for the background file.

--- js/background.js
+++ js/background.js
@@ -1,11 +1,14 @@
+// An array of three different images.
 const images = [
     "0.jpg",
     "1.jpg",
     "2.jpg"
 ];
 
+// Like the quotes, we use the floor function to choose a random image.
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+// Displays our random image.
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
